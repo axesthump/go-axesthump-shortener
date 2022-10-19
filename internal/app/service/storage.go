89835b4a8af991dc76b60334503ddc9a8b4be5eb
axesthump <mrsalamon77@gmail.com>
2022-10-19 21:17:44 +0300

    Increment1 вынес сервис в отдельный пакет

--- internal/app/service/storage.go
+++ internal/app/service/storage.go
@@ -8,32 +8,32 @@ import (
 type Storage struct {
 	mx     *sync.RWMutex
 	urls   map[int64]string
-	lastID int64
+	lastId int64
 }
 
 func NewStorage() *Storage {
 	return &Storage{
 		mx:     &sync.RWMutex{},
 		urls:   make(map[int64]string, 0),
-		lastID: int64(0),
+		lastId: int64(0),
 	}
 }
 
-func (s *Storage) CreateShortURL(url string) (shortURL int64) {
+func (s *Storage) CreateShortUrl(url string) (shortUrl int64) {
 	s.mx.Lock()
-	s.urls[s.lastID] = url
+	s.urls[s.lastId] = url
 	s.mx.Unlock()
 
-	shortURL = s.lastID
-	s.lastID++
-	return shortURL
+	shortUrl = s.lastId
+	s.lastId++
+	return shortUrl
 }
 
-func (s *Storage) GetFullURL(shortURL int64) (string, error) {
+func (s *Storage) GetFullUrl(shortUrl int64) (string, error) {
 	s.mx.RLock()
 	defer s.mx.RUnlock()
-	if longURL, ok := s.urls[shortURL]; ok {
-		return longURL, nil
+	if longUrl, ok := s.urls[shortUrl]; ok {
+		return longUrl, nil
 	} else {
 		return "", fmt.Errorf("url dont exist")
 	}
