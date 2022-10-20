0c5c0d89b80036c5e8c2e0955a27148baf8c905a
axesthump <mrsalamon77@gmail.com>
2022-10-20 15:06:43 +0300

    Increment2 добавил тесты

---
+++ internal/app/repository/storage_test.go
@@ -0,0 +1,130 @@
+package repository
+
+import (
+	"github.com/stretchr/testify/assert"
+	"sync"
+	"testing"
+)
+
+func TestStorage_CreateShortURL(t *testing.T) {
+	type fields struct {
+		mx     *sync.RWMutex
+		urls   map[int64]string
+		lastID int64
+	}
+	type args struct {
+		beginURL string
+		url      string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   string
+	}{
+		{
+			name: "check success create",
+			fields: fields{
+				mx:     &sync.RWMutex{},
+				urls:   map[int64]string{},
+				lastID: 0,
+			},
+			args: args{
+				beginURL: "http://begin:8080/",
+				url:      "http://begin:8080/some/path",
+			},
+			want: "http://begin:8080/0",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Storage{
+				mx:     tt.fields.mx,
+				urls:   tt.fields.urls,
+				lastID: tt.fields.lastID,
+			}
+			got := s.CreateShortURL(tt.args.beginURL, tt.args.url)
+			if got != tt.want {
+				t.Errorf("CreateShortURL() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStorage_GetFullURL(t *testing.T) {
+	type fields struct {
+		mx     *sync.RWMutex
+		urls   map[int64]string
+		lastID int64
+	}
+	type args struct {
+		shortURL int64
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "check success",
+			fields: fields{
+				mx:     &sync.RWMutex{},
+				urls:   map[int64]string{0: "fullURL"},
+				lastID: 1,
+			},
+			args: args{
+				shortURL: int64(0),
+			},
+			want:    "fullURL",
+			wantErr: false,
+		},
+		{
+			name: "check fail",
+			fields: fields{
+				mx:     &sync.RWMutex{},
+				urls:   map[int64]string{0: "fullURL"},
+				lastID: 1,
+			},
+			args: args{
+				shortURL: int64(1),
+			},
+			want:    "",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Storage{
+				mx:     tt.fields.mx,
+				urls:   tt.fields.urls,
+				lastID: tt.fields.lastID,
+			}
+			got, err := s.GetFullURL(tt.args.shortURL)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetFullURL() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("GetFullURL() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStorage_CreateShortURLDoubleCheck(t *testing.T) {
+	s := &Storage{
+		mx:     &sync.RWMutex{},
+		urls:   map[int64]string{},
+		lastID: 0,
+	}
+	beginURL := "http://begin:8080/"
+	fullURL := beginURL + "some/path"
+	fullURL2 := beginURL + "some/path/path"
+	got := s.CreateShortURL(beginURL, fullURL)
+	assert.Equal(t, beginURL+"0", got)
+	got = s.CreateShortURL(beginURL, fullURL2)
+	assert.Equal(t, beginURL+"1", got)
+
+}
