1e6f8cd161ccc3d36ed0a48d0c24afa8bdc7b153
axesthump <mrsalamon77@gmail.com>
2022-10-22 14:52:49 +0300

    Increment5 добавил поддержку настройки сервера через переменные окружения

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"github.com/caarlos0/env/v6"
 	"go-axesthump-shortener/internal/app/handlers"
 	"go-axesthump-shortener/internal/app/repository"
 	"log"
 	"net/http"
 )
 
-const (
-	serverURL = "localhost:8080"
-	protocol  = "http://"
-)
+type serverConfig struct {
+	ServerAddr string `env:"SERVER_ADDRESS"`
+	BaseUrl    string `env:"BASE_URL"`
+}
 
 func main() {
-	appHandler := handlers.NewAppHandler(protocol+serverURL+"/", repository.NewStorage())
-	log.Fatal(http.ListenAndServe(serverURL, appHandler.Router))
+	serverConfig := serverConfig{}
+	err := env.Parse(&serverConfig)
+	if err != nil {
+		panic(err)
+	}
+	appHandler := handlers.NewAppHandler(serverConfig.BaseUrl, repository.NewStorage())
+	log.Fatal(http.ListenAndServe(serverConfig.ServerAddr, appHandler.Router))
 }
