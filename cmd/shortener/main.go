6744a0dbc596028e71c304b8b97b0bf5221fc3cd
axesthump <mrsalamon77@gmail.com>
2022-10-23 20:14:44 +0300

    Increment5 поправил baseURL

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -10,8 +10,8 @@ import (
 
 func main() {
 	serverAddr := util.GetEnvOrDefault("SERVER_ADDRESS", "localhost:8080")
-	baseURL := util.GetEnvOrDefault("BASE_URL", "http://localhost:8080/")
+	baseURL := util.GetEnvOrDefault("BASE_URL", "http://localhost:8080")
 
-	appHandler := handlers.NewAppHandler(baseURL, repository.NewStorage())
+	appHandler := handlers.NewAppHandler(baseURL+"/", repository.NewStorage())
 	log.Fatal(http.ListenAndServe(serverAddr, appHandler.Router))
 }
