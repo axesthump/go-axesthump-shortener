6d2a6a2fb4f2456edb2bc58a2a8f6a36bdae2ba0
axesthump <mrsalamon77@gmail.com>
2022-10-25 19:48:50 +0300

    Increment7 добавил поддержку аргументов командной строки

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -1,35 +1,51 @@
 package config
 
 import (
+	"flag"
 	"go-axesthump-shortener/internal/app/repository"
 	"go-axesthump-shortener/internal/app/util"
 	"os"
 )
 
 type AppConfig struct {
-	ServerAddr string
-	BaseURL    string
-	Repo       repository.Repository
+	ServerAddr  string
+	BaseURL     string
+	Repo        repository.Repository
+	storagePath string
 }
 
 func CreateAppConfig() (*AppConfig, error) {
-	serverAddr := util.GetEnvOrDefault("SERVER_ADDRESS", "localhost:8080")
-	baseURL := util.GetEnvOrDefault("BASE_URL", "http://localhost:8080")
-	storagePath := os.Getenv("FILE_STORAGE_PATH")
+	appConfig := getConsoleArgs()
+	if appConfig.BaseURL == "" {
+		appConfig.BaseURL = util.GetEnvOrDefault("BASE_URL", "http://localhost:8080")
+	}
+	if appConfig.ServerAddr == "" {
+		appConfig.ServerAddr = util.GetEnvOrDefault("SERVER_ADDRESS", "localhost:8080")
+	}
+	if appConfig.storagePath == "" {
+		os.Getenv("FILE_STORAGE_PATH")
+	}
 
-	var repo repository.Repository
 	var err error
-	if len(storagePath) == 0 {
-		repo = repository.NewInMemoryStorage()
+	if len(appConfig.storagePath) == 0 {
+		appConfig.Repo = repository.NewInMemoryStorage()
 	} else {
-		repo, err = repository.NewLocalStorage(storagePath)
+		appConfig.Repo, err = repository.NewLocalStorage(appConfig.storagePath)
 	}
 	if err != nil {
 		return nil, err
 	}
+	return appConfig, nil
+}
+
+func getConsoleArgs() *AppConfig {
+	serverAddr := flag.String("a", "", "server address")
+	baseURL := flag.String("b", "", "base url")
+	storagePath := flag.String("f", "", "storage path")
+
 	return &AppConfig{
-		ServerAddr: serverAddr,
-		BaseURL:    baseURL,
-		Repo:       repo,
-	}, nil
+		ServerAddr:  *serverAddr,
+		BaseURL:     *baseURL,
+		storagePath: *storagePath,
+	}
 }
