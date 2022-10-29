b970046bb02135949ba98aa1b163b9233ea26107
axesthump <mrsalamon77@gmail.com>
2022-10-29 18:40:09 +0300

    Increment9 поправил обработку ошибки закрытия

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,22 +3,35 @@ package main
 import (
 	"go-axesthump-shortener/internal/app/config"
 	"go-axesthump-shortener/internal/app/handlers"
-	"go-axesthump-shortener/internal/app/repository"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
+
+func handleShutdown(signalHandler chan os.Signal, done chan bool, conf *config.AppConfig) {
+	<-signalHandler
+	err := conf.Repo.Close()
+	if err != nil {
+		panic(err)
+	}
+	done <- true
+}
 
 func main() {
 	conf, err := config.CreateAppConfig()
-	defer func(Repo repository.Repository) {
-		err := Repo.Close()
-		if err != nil {
-			panic(err)
-		}
-	}(conf.Repo)
+	signalHandler := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	signal.Notify(signalHandler, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 	if err != nil {
 		panic(err)
 	}
 	appHandler := handlers.NewAppHandler(conf.BaseURL+"/", conf.Repo)
-	log.Fatal(http.ListenAndServe(conf.ServerAddr, appHandler.Router))
+	go handleShutdown(signalHandler, done, conf)
+	go func() {
+		log.Fatal(http.ListenAndServe(conf.ServerAddr, appHandler.Router))
+	}()
+	<-done
+	os.Exit(0)
 }
